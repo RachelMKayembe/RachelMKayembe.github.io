f26c703eef5c30b35b71b0cea708f94708a516c0
RachelMKayembe <65307213+RachelMKayembe@users.noreply.github.com>
2023-06-10 23:24:54 -0400

    push

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,14 +7,21 @@ navLinkEls.forEach(navLinkEl => {
     }
 });  
 
-function submitBooking(){
-    var firstName = document.getElementById('firstname').value;
-    var phoneNum = document.getElementById('phone').value;
-    var pickedDate = document.getElementById('datebook').value;
-    var delService = document.getElementById('selectService').value;
-    var lastName = document.getElementById('lastName').value;
-    var emailAdress = document.getElementById('emailAd').value;
-    var stylist = document.getElementById('pickStylist').value;
+// function submitBooking(){
+//     const firstName = document.getElementById('firstname').value;
+//     var phoneNum = document.getElementById('phone').value;
+//     var pickedDate = document.getElementById('datebook').value;
+//     var delService = document.getElementById('selectService').value;
+//     var lastName = document.getElementById('lastName').value;
+//     var emailAdress = document.getElementById('emailAd').value;
+//     var stylist = document.getElementById('pickStylist').value;
   
-    window.alert("Thank you " + firstName + " for booking with " + stylist + "... A confirmation Email has been sent to: " + emailAdress);
-  }
+//     window.alert("Thank you " + firstName + " for booking with " + stylist + "... A confirmation Email has been sent to: " + emailAdress);
+//   }
+
+(function () {
+    const button = document.getElementById('submit-btn');
+    button.addEventListener('click',function() {
+        alert('Im the behavior layer');
+    });
+})();
