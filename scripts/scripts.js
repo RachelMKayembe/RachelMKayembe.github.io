e1c699697f67a8a8d1540e3e7892db67cb5b0215
RachelMKayembe <65307213+RachelMKayembe@users.noreply.github.com>
2023-06-11 12:49:22 -0400

    ff

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,29 @@
-const navLinkEls = Document.querySelectorAll('.nav-link');
-const windowPathname = window.location.pathname;
+// const navLinkEls = Document.querySelectorAll('.nav-link');
+// const windowPathname = window.location.pathname;
 
-navLinkEls.forEach(navLinkEl => {
-    if (navLinkEl.href.includes(windowPathname)) {
-    navLinkEl.classList.add('active');
-    }
-});  
+// navLinkEls.forEach(navLinkEl => {
+//     if (navLinkEl.href.includes(windowPathname)) {
+//     navLinkEl.classList.add('active');
+//     }
+// });  
 
-// function submitBooking(){
-//     const firstName = document.getElementById('firstname').value;
-//     var phoneNum = document.getElementById('phone').value;
-//     var pickedDate = document.getElementById('datebook').value;
-//     var delService = document.getElementById('selectService').value;
-//     var lastName = document.getElementById('lastName').value;
-//     var emailAdress = document.getElementById('emailAd').value;
-//     var stylist = document.getElementById('pickStylist').value;
+// // function submitBooking(){
+// //     const firstName = document.getElementById('firstname').value;
+// //     var phoneNum = document.getElementById('phone').value;
+// //     var pickedDate = document.getElementById('datebook').value;
+// //     var delService = document.getElementById('selectService').value;
+// //     var lastName = document.getElementById('lastName').value;
+// //     var emailAdress = document.getElementById('emailAd').value;
+// //     var stylist = document.getElementById('pickStylist').value;
   
-//     window.alert("Thank you " + firstName + " for booking with " + stylist + "... A confirmation Email has been sent to: " + emailAdress);
-//   }
+// //     window.alert("Thank you " + firstName + " for booking with " + stylist + "... A confirmation Email has been sent to: " + emailAdress);
+// //   }
 
-(function () {
-    const button = document.getElementById('submit-btn');
-    button.addEventListener('click',function() {
-        alert('Im the behavior layer');
-    });
-})();
+// (function () {
+//     const button = document.getElementById('submit-btn');
+//     button.addEventListener('click',function() {
+//         alert('Im the behavior layer');
+//     });
+// })();
+
+alert('test')
